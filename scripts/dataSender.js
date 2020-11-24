8902a208c8699a6bc0f74a3380712214534edd5b
OrbitalMechanist <pilot.max777@gmail.com>
2020-11-23 17:23:20 -0800

    Merge orbital-work into main with question writing.

--- scripts/dataSender.js
+++ scripts/dataSender.js
@@ -1,10 +1,6 @@
 // JavaScript source code
 function sendToQuestionnaire(questionnaire, question, textToSet) {
-<<<<<<< HEAD
-    db.collection("creators").doc("test").collection("questionnaires").doc(questionnaire).
-=======
     db.collection("creators").doc(auth.currentUser.uid).collection("questionnaires").doc(questionnaire).
->>>>>>> orbital-work
         collection("questions").doc(question).set({
             questionText: textToSet
         })
